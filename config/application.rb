d1dbda7a31a647b42c0d8e398a3894ae69c947bc
Marc Blümner <m.bluemner@liquidlabs.de>
2014-06-13 18:59:01 +0200

    finished user signup and admin box

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module CucumberTester
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
-        controller_specs: false,
+        controller_specs:true, 
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
